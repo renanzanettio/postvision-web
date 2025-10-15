584b344ba65d2bfaa7e71d8d77e4e3db9c0dde3a
Renan Zanetti <93152181+renanzanettio@users.noreply.github.com>
2025-10-15 08:20:43 -0300

    Update import jsonwebtoken from middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import jwt from "jsonwebtoken";
+import * as jwt from "jsonwebtoken";
 
 const SECRET = process.env.JWT_SECRET || "segredo";
 
